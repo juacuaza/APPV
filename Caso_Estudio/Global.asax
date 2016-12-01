f49d4662cf216a61da2faaab90c434356d2db4c5
Juan Manuel Cuartas <usu4@etsinf.upv.es>
2016-12-01 17:56:52 +0100

    Agregar archivos de proyecto.

---
+++ Caso_Estudio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Caso_Estudio.MvcApplication" Language="C#" %>
---
+++ Caso_Estudio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Caso_Estudio.Startup))]
+namespace Caso_Estudio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
